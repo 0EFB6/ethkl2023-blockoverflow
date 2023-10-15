dec8e666df3820c87ae4ba5f68bf22a52ab9addc
ShadowofSkull <zhplana@gmail.com>
2023-10-15 09:44:39 +0800

    add mantle logo and claim insurance func

--- mantle/test_frontend/src/App.js
+++ mantle/test_frontend/src/App.js
@@ -9,11 +9,11 @@ import Funds from "./Funds";
 import DIS from "./artifacts/DIS.json";
 import { utils, BigNumber } from "ethers" 
 // The contract address
-const greeterAddress = "0x4e25F10b3C81cf474E4361C109dbF7901B3dDBA8";
+const contractAddress = "0x4e25F10b3C81cf474E4361C109dbF7901B3dDBA8";
 
 function App() {
   // Property Variables
-
+  const [address, setAddress] = useState("");
   const [currencyCode, setCurrencyCode] = useState("");
   const [companyName, setCompanyName] = useState("");
   const [exTotalFund, setExTotalFund] = useState("");
@@ -38,7 +38,7 @@ function App() {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const signer = provider.getSigner();
 
-      const contract = new ethers.Contract(greeterAddress, DIS.abi, signer);
+      const contract = new ethers.Contract(contractAddress, DIS.abi, signer);
       try {
         // Call contract function
         const transaction = await contract.payPremium(exTotalFund, companyName, currencyCode, {value: 2});
@@ -57,12 +57,40 @@ function App() {
     }
   }
 
+  async function callClaimInsurance() {
+    if (!address) return;
+
+    // If MetaMask exists
+    if (typeof window.ethereum !== "undefined") {
+      await requestAccount();
+
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+
+      const contract = new ethers.Contract(contractAddress, DIS.abi, signer);
+      try {
+        // Call contract function
+        const transaction = await contract.claimInsurance(address);
+        // value are in wei, 1 ether is 1e18 wei
+        // console.log(transaction)
+        // Clear value after user call the function so user can set again
+        setAddress("");
+
+
+        await transaction.wait();
+      } catch (error) {
+        console.log("Error: ", error);
+      
+      }
+    }
+  }
+
   async function getTotalFunds() {
     // If MetaMask exists
     if (typeof window.ethereum !== "undefined") {
       const provider = new ethers.providers.Web3Provider(window.ethereum);
       const contract = new ethers.Contract(
-        greeterAddress,
+        contractAddress,
         DIS.abi,
         provider
       );
@@ -93,34 +121,47 @@ function App() {
         </Routes>
       </article>
       <Footer/>
-      <div className="App-header">
+      {/* <div className="App-header"> */}
         {/* BUTTONS - Fetch and Set */}
-        <div className="custom-buttons">
+        {/* <div className="custom-buttons">
           <button onClick={getTotalFunds} style={{ backgroundColor: "green" }}>
             Fetch Fund
           </button>
           <button onClick={callPayPremium} style={{ backgroundColor: "red" }}>
             Pay Premium
           </button>
-        </div>
+          <button onClick={callClaimInsurance} style={{ backgroundColor: "green" }}>
+            Claim Insurance
+          </button>
+        </div> */}
 
         {/* INPUT TEXT - String  */}
-        <input
-          onChange={(e) => setCurrencyCode(e.target.value)}
-          value={currencyCode}
-          placeholder="Set Currency Code"
-        />
-        <input
-          onChange={(e) => setCompanyName(e.target.value)}
-          value={companyName}
-          placeholder="Set Company Name"
-        />
-        <input
-          onChange={(e) => setExTotalFund(e.target.value)}
-          value={exTotalFund}
-          placeholder="Set Total fund"
-        />
-      </div>
+        {/* <div className="premium">
+          <input
+            onChange={(e) => setCurrencyCode(e.target.value)}
+            value={currencyCode}
+            placeholder="Set Currency Code"
+          />
+          <input
+            onChange={(e) => setCompanyName(e.target.value)}
+            value={companyName}
+            placeholder="Set Company Name"
+          />
+          <input
+            onChange={(e) => setExTotalFund(e.target.value)}
+            value={exTotalFund}
+            placeholder="Set Total fund"
+          />
+        </div>
+        <div className="claim">
+          <input
+            onChange={(e) => setAddress(e.target.value)}
+            value={address}
+            placeholder="Set Address"
+          />
+        </div> */}
+        
+      {/* </div> */}
     </div>
   );
 }
